001978f98b6dd74e446816869269c0b89a2fd686
James-Shadrack-Wafula <jamesshadrackwafula.gmail.com>
2024-04-02 16:31:03 +0300

    Configured Hosts

--- unisell/settings.py
+++ unisell/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true" 
 
 
-# ALLOWED_HOSTS = ['*'] 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*'] 
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
